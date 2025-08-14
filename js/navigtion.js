84bd6da26232876ad9a1d6a10334036589396ca2
FREDWIL MEMBRERE <fredwilmembrere@gmail.com>
2025-08-14 16:35:56 +0800

    First Commit

---
+++ js/navigtion.js
@@ -0,0 +1,26 @@
+
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const id = entry.target.getAttribute("id");
+        const navLink = document.querySelector(`.nav-link[href="#${id}"]`);
+
+        if (entry.isIntersecting) {
+          navLinks.forEach((link) => link.classList.remove("active"));
+          navLink.classList.add("active");
+        }
+      });
+    },
+    {
+      threshold: 0.6, // 60% of section must be visible to trigger
+    }
+  );
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+
+  document.getElementById('fb').alt = "Anything";
